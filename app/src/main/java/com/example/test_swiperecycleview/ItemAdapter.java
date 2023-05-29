dfc14e0803459af060aabc59ed96313e946bf71b
tungnk123 <doanthanhtungnk123@gmail.com>
2023-05-29 23:10:47 +0700

    Change color and icon for each button

--- app/src/main/java/com/example/test_swiperecycleview/ItemAdapter.java
+++ app/src/main/java/com/example/test_swiperecycleview/ItemAdapter.java
@@ -1,15 +1,23 @@
 package com.example.test_swiperecycleview;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
 
     private List<String> itemList;
 
@@ -28,18 +36,47 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
         String itemName = itemList.get(position);
         holder.textViewItemName.setText(itemName);
-
-//        holder.buttonAction.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Handle button click event
-//                // Perform the desired action for the button
-//            }
-//        });
     }
 
     @Override
     public int getItemCount() {
         return itemList.size();
+    }
+
+    public void removeItem(int position) {
+        itemList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void checkDoneItem(int position) {
+        // Perform check done action
+        // You can update the item status or perform any other necessary operations
+        itemList.remove(position);
+        notifyItemChanged(position);
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewItemName;
+        public Button buttonDelete;
+        public Button buttonCheckDone;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textViewItemName = itemView.findViewById(R.id.textViewItemName);
+            buttonDelete = itemView.findViewById(R.id.buttonDelete);
+            buttonCheckDone = itemView.findViewById(R.id.buttonCheckDone);
+
+            // Set different icons and colors for buttons
+            Drawable deleteIcon = ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_launcher_background);
+            Drawable checkDoneIcon = ContextCompat.getDrawable(itemView.getContext(), R.drawable.baseline_done_24);
+            int deleteColor = ContextCompat.getColor(itemView.getContext(), R.color.purple_200);
+            int checkDoneColor = ContextCompat.getColor(itemView.getContext(), R.color.teal_200);
+
+            buttonDelete.setCompoundDrawablesWithIntrinsicBounds(deleteIcon, null, null, null);
+            buttonCheckDone.setCompoundDrawablesWithIntrinsicBounds(checkDoneIcon, null, null, null);
+            buttonDelete.setBackgroundColor(deleteColor);
+            buttonCheckDone.setBackgroundColor(checkDoneColor);
+        }
     }
 }
--- app/src/main/java/com/example/test_swiperecycleview/MainActivity.java
+++ app/src/main/java/com/example/test_swiperecycleview/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.test_swiperecycleview;
 
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,46 +34,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the RecyclerView and item list
         recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         itemList = new ArrayList<>();
         itemList.add("Item 1");
         itemList.add("Item 2");
         itemList.add("Item 3");
 
-        // Set up the RecyclerView with LinearLayoutManager
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // Create the adapter and set it to the RecyclerView
         itemAdapter = new ItemAdapter(itemList);
         recyclerView.setAdapter(itemAdapter);
 
-        // Set up swipe-to-check-done and swipe-to-delete functionality
-        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                int position = viewHolder.getAdapterPosition();
-                if (direction == ItemTouchHelper.LEFT) {
-                    // Handle left swipe (check done action)
-                    // Perform the "check done" action for the item at the position
-                    // Update the item's status or perform any other action
-                } else if (direction == ItemTouchHelper.RIGHT) {
-                    // Handle right swipe (delete action)
-                    // Perform the "delete" action for the item at the position
-                    // Remove the item from the list and update the adapter
-                    itemList.remove(position);
-                    itemAdapter.notifyItemRemoved(position);
-                }
-            }
-        };
-
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        // Set up swipe functionality
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeToDeleteCallback());
         itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+
+        private Drawable deleteIcon;
+        private Drawable checkDoneIcon;
+        private final ColorDrawable deleteBackground;
+        private final ColorDrawable checkDoneBackground;
+
+        public SwipeToDeleteCallback() {
+            super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+            deleteIcon = ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher_background);
+            checkDoneIcon = ContextCompat.getDrawable(MainActivity.this, R.drawable.baseline_done_24);
+            deleteBackground = new ColorDrawable(Color.RED);
+            checkDoneBackground = new ColorDrawable(Color.GREEN);
+        }
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            if (direction == ItemTouchHelper.LEFT) {
+                itemAdapter.removeItem(position);
+                Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+            } else if (direction == ItemTouchHelper.RIGHT) {
+                itemAdapter.checkDoneItem(position);
+                Toast.makeText(MainActivity.this, "Check Done", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            View itemView = viewHolder.itemView;
+            int backgroundCornerOffset = 20;
+
+            if (dX > 0) { // Swiping to the right (Check Done)
+                int iconMargin = (itemView.getHeight() - checkDoneIcon.getIntrinsicHeight()) / 2;
+                int iconTop = itemView.getTop() + (itemView.getHeight() - checkDoneIcon.getIntrinsicHeight()) / 2;
+                int iconBottom = iconTop + checkDoneIcon.getIntrinsicHeight();
+
+                int iconLeft = itemView.getLeft() + iconMargin;
+                int iconRight = itemView.getLeft() + iconMargin + checkDoneIcon.getIntrinsicWidth();
+                checkDoneIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                checkDoneBackground.setBounds(itemView.getLeft(), itemView.getTop(),
+                        itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
+            } else if (dX < 0) { // Swiping to the left (Delete)
+                int iconMargin = (itemView.getHeight() - deleteIcon.getIntrinsicHeight()) / 2;
+                int iconTop = itemView.getTop() + (itemView.getHeight() - deleteIcon.getIntrinsicHeight()) / 2;
+                int iconBottom = iconTop + deleteIcon.getIntrinsicHeight();
+
+                int iconLeft = itemView.getRight() - iconMargin - deleteIcon.getIntrinsicWidth();
+                int iconRight = itemView.getRight() - iconMargin;
+                deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                deleteBackground.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                        itemView.getTop(), itemView.getRight(), itemView.getBottom());
+            } else { // View is unswiped
+                deleteBackground.setBounds(0, 0, 0, 0);
+                checkDoneBackground.setBounds(0, 0, 0, 0);
+            }
+
+            deleteBackground.draw(c);
+            deleteIcon.draw(c);
+            checkDoneBackground.draw(c);
+            checkDoneIcon.draw(c);
+        }
     }
 }
